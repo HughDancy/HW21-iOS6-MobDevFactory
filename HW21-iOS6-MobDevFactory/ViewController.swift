5c30386cdd52324ad5045252046730c4a00eaaa5
BorisKiselev <hugh.dancy@icloud.com>
2022-07-14 00:38:59 +0700

    delete

--- HW21-iOS6-MobDevFactory/ViewController.swift
+++ HW21-iOS6-MobDevFactory/ViewController.swift
@@ -69,13 +69,13 @@ class ViewController: UIViewController {
 
         var password: String = ""
 
-        // Will strangely ends at 0000 instead of ~~~
-        while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
+       
+        while password != passwordToUnlock {
             password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
-//             Your stuff here
+
             
             print(password)
-            // Your stuff here
+      
         }
         
         print(password)
